ab59cb3dcafe3a629ac3a3c1139f0e3c297edefd
wtbno <bruno_links@hotmail.com>
2023-09-09 10:53:51 -0300

    funções

--- Funções/funcoes.py
+++ Funções/funcoes.py
@@ -8,7 +8,7 @@ Por padrão, funções Python retornam None (nada).
 """
 
 
-# def Print(a, b, c):
+# def Print(a, b, c): => seria um parâmetro
 #     print('Várias1')
 #     print('Várias2')
 #     print('Várias3')
@@ -18,14 +18,16 @@ Por padrão, funções Python retornam None (nada).
 #     print(a, b, c)
 
 
-# imprimir(1, 2, 3)
+# imprimir(1, 2, 3) => argumentos da variável, ou valores
 # imprimir(4, 5, 6)
+# None é como se fosse um false
+
 
 def saudacao(nome='Sem nome'):
     print(f'Olá, {nome}!')
 
 
-saudacao('Luiz Otávio')
-saudacao('Maria')
-saudacao('Helena')
-saudacao()
+saudacao('Bruno')
+saudacao('Camila')
+saudacao('Shen')
+saudacao()  # => caso esteja sem valor ele irá retornar o 'Sem nome' que está definido acima
