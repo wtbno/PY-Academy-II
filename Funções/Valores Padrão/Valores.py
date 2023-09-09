507743331eacb697d1b8600c912bb2666d62d3a2
wtbno <bruno_links@hotmail.com>
2023-09-09 17:01:54 -0300

    Valores padrão

---
+++ Funções/Valores Padrão/Valores.py
@@ -0,0 +1,10 @@
+def soma(x, y, z=None):  # => None é o valor padrão
+    if z is not None:
+        print(f'{x=} {y=} {z=}', x + y + z)
+    else:
+        print(f'{x=} {y=}', x + y)
+
+
+soma(1, 2)
+soma(3, 5)
+soma(895, 931)
